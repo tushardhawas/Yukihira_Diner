45a5414a596abfc68fcddaafcea8c3665daafd9b
tushardhawas <tushardhawasssd@gmail.com>
2024-08-09 23:46:18 +0530

    live api data

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,7 +1,19 @@
 import { useEffect, useState } from "react";
 
+const RestroCard = ({}) => {
+  return (
+    <div className="card">
+      <img src={IMG_CDN_URL + i.cloudinaryImageId}></img>
+      <h4>{i.name}</h4>
+      <h6 style={{ color: "red" }}>⭐{i.avgRating}</h6>
+      <h6> {i.cuisines.join(",")}</h6>
+      <h6> {i.locality}</h6>
+    </div>
+  );
+};
+
 const Body = () => {
-  const { restaurant, setRestaurant } = useState("Restarunt");
+  const [restaurant, setRestaurant] = useState("Restarunt");
 
   useEffect(() => {
     async function getData() {
@@ -13,13 +25,18 @@ const Body = () => {
     }
 
     const data = getData().then((result) => {
-      console.log(result);
+      setRestaurant(result);
     });
-
-    console.log("useeffect");
   }, []);
-  console.log("render");
 
-  return <>{restaurant}</>;
+  return (
+    <>
+      <div className="cards">
+        {restaurant.map((rest, index) => (
+          <RestroCard key={index} i={rest.info} />
+        ))}
+      </div>
+    </>
+  );
 };
 export default Body;
