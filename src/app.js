fd1c5e3aaf9b4aa35b0f0a139e0a3655425e65bf
tushardhawas <tushardhawasssd@gmail.com>
2024-08-12 23:30:09 +0530

    adding router

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom/client";
 import Navbar from "./component/Header";
 import Body from "./component/Body";
 import Footer from "./component/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const appRouter = createBrowserRouter([{path:"/",element:<AppLayout/>}, {}]);
 const AppLayout = () => {
   return (
     <>
